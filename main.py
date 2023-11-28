869fac9628946a1216993f3ea7c1fb9083ec40ba
bogdan01official <108180181+bogdan01official@users.noreply.github.com>
2023-11-28 23:19:57 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# from tkinter import *
+#
+# root = Tk()
+# root.title("Приложение на Tkinter")
+#
+# root.geometry("300x250")
+#
+# label = Label(text="Hello РУКОН ЦЕЛЬ")
+# label.pack()
+#
+# root.mainloop()
+
+
+# Счетчик суммы и разности
+# import tkinter as tk
+#
+#
+# def increment_counter():
+#     global counter
+#     counter += 1
+#     counter_label.config(text=f"Сумма: {counter}")
+#
+#
+# def decrement_counter():
+#     global counter
+#     counter -= 1
+#     counter_label.config(text=f"Разность: {counter}")
+#
+#
+# counter = 0
+#
+# window = tk.Tk()
+# window.title("Счетчик суммы и разности")
+# window.geometry("350x100")
+#
+# counter_label = tk.Label(window, text="Счетчик: 0")
+# counter_label.pack()
+#
+# increment_button = tk.Button(window, text="Сумма", command=increment_counter)
+# increment_button.pack()
+#
+# decrement_button = tk.Button(window, text="Сумма", command=decrement_counter)
+# decrement_button.pack()
+#
+# window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
